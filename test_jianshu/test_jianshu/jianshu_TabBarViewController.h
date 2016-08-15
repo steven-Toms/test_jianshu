75a7d9901efb50ff7280663e28b7e325132d7115
Steven <fgh393794179@163.com>
2016-08-16 05:56:49 +0800

    tabbar 8.16.10.00
    tabbar

---
+++ test_jianshu/test_jianshu/jianshu_TabBarViewController.h
@@ -0,0 +1,13 @@
+//
+//  jianshu_TabBarViewController.h
+//  test_jianshu
+//
+//  Created by mac-Win on 16/8/16.
+//  Copyright © 2016年 mac-Win. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface jianshu_TabBarViewController : UITabBarController
+
+@end
---
+++ test_jianshu/test_jianshu/jianshu_TabBarViewController.m
@@ -0,0 +1,37 @@
+//
+//  jianshu_TabBarViewController.m
+//  test_jianshu
+//
+//  Created by mac-Win on 16/8/16.
+//  Copyright © 2016年 mac-Win. All rights reserved.
+//
+
+#import "jianshu_TabBarViewController.h"
+
+@interface jianshu_TabBarViewController ()
+
+@end
+
+@implementation jianshu_TabBarViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
